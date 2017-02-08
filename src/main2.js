4918fc2c70d415d25f526fe92fd681ddc030e91e
lincolnge <326684793@qq.com>
2017-02-08 17:37:33 +0800

    Update: 分离 decorator.

--- src/main2.js
+++ src/main2.js
@@ -11,25 +11,3 @@ console.log('NaN', _.isNaN(NaN));
 
 var $ = require('jquery');
 console.log('$', $('body'));
-
-// 测试 Decorator
-// http://es6.ruanyifeng.com/#docs/decorator
-
-function testable(target) {
-  target.isTestable = true;
-}
-
-@testable
-class MyTestableClass {}
-
-console.log('MyTestableClass.isTestable', MyTestableClass.isTestable) // true
-
-/*
-@decorator
-class A {}
-
-// 等同于
-
-class A {}
-A = decorator(A) || A;
-*/
---
+++ src/test_decorator.js
@@ -0,0 +1,28 @@
+// 测试 Decorator
+// http://es6.ruanyifeng.com/#docs/decorator
+console.log('hello, decorator!');
+
+function testable(isTestable) {
+  return function(target) {
+    target.isTestable = isTestable;
+  }
+}
+
+@testable(true)
+class MyTestableClass {}
+console.log('MyTestableClass.isTestable', MyTestableClass.isTestable) // true
+
+@testable(false)
+class MyClass {}
+
+console.log('MyClass.isTestable', MyClass.isTestable) // true
+
+/*
+@decorator
+class A {}
+
+// 等同于
+
+class A {}
+A = decorator(A) || A;
+*/
