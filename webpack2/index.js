20b0466976ca514eac85cd4c8e57609d5dd30b40
lincolnge <326684793@qq.com>
2017-03-30 21:31:53 +0800

    Update: fix bug, 配置 cache control、Expires 还有 last modify。

--- webpack2/index.js
+++ webpack2/index.js
@@ -28,6 +28,22 @@ const root = path.resolve(process.argv[2] || './dist');
 
 console.log('Static root dir: ' + root);
 
+
+const cacheConfig = {
+  '1-': {
+    "Cache-Control": `max-age=${10}`, // 设置 10 秒。
+  },
+  'index-': {
+    // 设置 Expires，即过期时间。
+    "Expires": new Date(Date.now() + 30 * 60 * 60 * 24 * 1000).toUTCString(),
+  },
+  // 'index2-': ((stats) => {
+  //   return {
+  //     "Last-Modified": stats && stats.mtime.toUTCString()
+  //   };
+  // }),
+};
+
 // 创建服务器
 const server = http.createServer(function (request, response) {
 
@@ -36,6 +52,7 @@ const server = http.createServer(function (request, response) {
 
   // 获得对应的本地文件路径，类似 '/srv/www/css/bootstrap.css'
   const filepath = path.join(root, pathname !== '/' ? pathname : 'index.html');
+  console.log('request url: "', request.url, '"\t ---\t', 'pathname', pathname);
 
   // 获取文件状态
   fs.stat(filepath, function (err, stats) {
@@ -45,12 +62,54 @@ const server = http.createServer(function (request, response) {
       console.log('200 ' + request.url);
 
       // 发送200响应
-      response.writeHead(200);
+      // response.writeHead(200, {
+      //   // 设置 Expires，即过期时间。
+      //   // "Expires": new Date(Date.now() + 30 * 60 * 60 * 24 * 1000).toUTCString(),
+      //   // "Expires": new Date(Date.now() + 3 * 1000).toUTCString(),
+      //   // "Cache-Control": 30 * 60 * 60 * 24 * 1000,
+      //   // "Cache-Control": `max-age=${10}`, // 设置 10 秒。
+      //   "Last-Modified": stats.mtime.toUTCString(),
+      // });
 
+      Object.keys(cacheConfig).forEach((reItem) => {
+        let options = cacheConfig[reItem];
+        if (typeof cacheConfig[reItem] === 'function') {
+          options = cacheConfig[reItem](stats);
+        }
+        if (new RegExp(reItem).test(pathname)) {
+          response.writeHead(200, options);
+        }
+      });
+
+      // check if if-modified-since header is the same as the mtime of the file
+      if (new RegExp('common').test(pathname)) {
+        let reqModDate = request.headers["if-modified-since"];
+        if (reqModDate !== null && reqModDate !== undefined) {
+          reqModDate = new Date(reqModDate);
+          console.log('reqModDate', reqModDate, 'reqModDate.getTime() === stats.mtime.getTime()', reqModDate.getTime() === stats.mtime.getTime());
+          if (reqModDate.getTime() === stats.mtime.getTime()) {
+            //Yes: then send a 304 header without image data (will be loaded by cache)
+            console.log("load from cache");
+            response.writeHead(304, {
+              "Last-Modified": stats.mtime.toUTCString()
+            });
+          }
+        } else {
+          // NO: then send the headers and the image
+          console.log("no cache");
+          response.writeHead(200, {
+            "Last-Modified": stats.mtime.toUTCString(),
+            "Content-Length": stats.size
+          });
+          // fs.createReadStream(filepath).pipe(response);
+        }
+      }
+
+      console.log('filepath', filepath, '\n\n');
       // 将文件流导向response
       fs.createReadStream(filepath).pipe(response);
-    } else {
 
+    } else {
       // 出错了或者文件不存在
       console.log('404 ' + request.url);
 
