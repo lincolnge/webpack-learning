75e20019a03d44bf85466c954575c6bccb6caffd
lincolnge <326684793@qq.com>
2017-11-20 17:17:04 +0800

    Update: update webpack2 config.

--- webpack2/webpack.config.js
+++ webpack2/webpack.config.js
@@ -8,13 +8,15 @@ const injectDevServer = require('./dev-server-inject');
 // 当访问 / 时，跳转到 /dist/index.html
 // injectDevServer();
 
-module.exports = {
-  // entry: ['./app/index.js'],
+const argvStr = process.argv.join();
+const isBuild = argvStr.indexOf('webpack') > -1 && argvStr.indexOf('webpack-dev-server') === -1;
+
+const webpackConfig = {
   entry: {
     index: [
+      // activate HMR for React
       'react-hot-loader/patch',
-      // // activate HMR for React
-      //
+
       // 'webpack-dev-server/client?http://localhost:8080',
       // // bundle the client for webpack-dev-server
       // // and connect to the provided endpoint
@@ -78,11 +80,17 @@ module.exports = {
     'react-dom': 'ReactDOM'
   },
   plugins: [
+    // 这个插件的作用是在热加载时直接返回更新文件名，而不是文件的id
     new webpack.NamedModulesPlugin(),
+    /**
+     * 模块热替换(HMR - Hot Module Replacement)功能会在应用程序运行过程中替换、添加或删除模块，而无需重新加载整个页面。
+     * https://doc.webpack-china.org/plugins/hot-module-replacement-plugin/
+     * 永远不要在生产环境(production)下启用 HMR？
+     */
     new webpack.HotModuleReplacementPlugin(),
-    new Visualizer({
-      filename: 'stats-view.html'
-    }),
+    // new Visualizer({
+    //   filename: 'stats-view.html'
+    // }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'index.html'),
       filename: 'index.html',
@@ -104,14 +112,10 @@ module.exports = {
   ],
   devServer: {
     compress: true,
-    hot: true,
-    // enable HMR on the server
+    hot: true, // enable HMR on the server
 
-    // contentBase: './dist',
-    // contentBase: false,
-    contentBase: path.join(__dirname, "dist"),
-    // contentBase: path.resolve(__dirname, 'dist'),
-    // match the output path
+    contentBase: path.resolve(__dirname, 'dist'), // match the output path
+    historyApiFallback: true,
 
     // publicPath: path.resolve(__dirname, 'dist'),
     publicPath: '/',
@@ -119,11 +123,24 @@ module.exports = {
       colors: true
     },
     host: '0.0.0.0',
-    port: 9999,
-    // proxy: {
-    //   [/\/(?!static).+/]: {
-    //     target: "http://localhost:8411",
-    //   }
-    // }
+    port: 9565,
+    proxy: [
+      {
+        // 使用 Staging 环境数据，注意不要污染线上数据
+        path: ['/api', '/backend', '/wakaka'],
+        // dev test url
+        target: 'http://127.0.0.1:8415'
+      }
+    ]
   }
 };
+
+
+if (!isBuild) {
+  // devtool 不要用 eval 模式，虽然这能带来初次构建减少 1s 左右的好处，
+  // 但是这会导致刷新页面中断无效，影响调试
+  // Chrome 升级49+ 后会导致 cheap-module-source-map 失效
+  // https://github.com/webpack/webpack/issues/2145
+  webpackConfig.devtool = '#cheap-module-source-map';
+}
+module.exports = webpackConfig;
