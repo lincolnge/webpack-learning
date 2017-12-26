aadf0b4b7dee5c5c75340319305c40f51e01d16f
lincolnge <326684793@qq.com>
2017-12-26 16:17:41 +0800

    Add: webpack2 支持修饰器。

--- webpack2/app/lib/lib/a.js
+++ webpack2/app/lib/lib/a.js
@@ -1,6 +1,19 @@
 var $ = require('jquery');
 console.log('$', $('body'));
 
+
+@testable
+class MyTestableClass {
+  // ...
+}
+
+function testable(target) {
+  target.isTestable = true;
+}
+
+console.log('MyTestableClass.isTestable', MyTestableClass.isTestable); // true
+
+
 module.exports = {
     sayHello: function(){
         console.log('common a 123!');
